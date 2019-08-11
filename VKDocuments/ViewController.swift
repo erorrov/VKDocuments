f2726b024dcd7203eb1acf5ec2b3e511c0b4a6e9
Egor Egorov <erorrov@gmail.com>
2019-08-11 17:20:30 +0300

    Initial Commit

---
+++ VKDocuments/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKDocuments
+//
+//  Created by Егор Егоров on 11/08/2019.
+//  Copyright © 2019 Егор Егоров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

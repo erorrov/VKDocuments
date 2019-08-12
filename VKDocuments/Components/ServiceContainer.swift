48cfcc5a0e002dbc75e572c74d5e7a29f138c22c
Egor Egorov <erorrov@gmail.com>
2019-08-12 20:17:32 +0300

    added service container

---
+++ VKDocuments/Components/ServiceContainer.swift
@@ -0,0 +1,24 @@
+//
+//  ServiceContainer.swift
+//  VKDocuments
+//
+//  Created by Егор Егоров on 12/08/2019.
+//  Copyright © 2019 Егор Егоров. All rights reserved.
+//
+
+import Foundation
+
+
+typealias SC = ServiceContainer
+
+final class ServiceContainer {
+    static let container = ServiceContainer()
+
+    private (set) var apiManager = APIManager()
+    private (set) var userManager = UserManager()
+    
+    static var services: ServiceContainer {
+        return SC.container
+    }
+}
+
